51d7ba123dba75bc752a86f9710ff423e7a68802
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2023-12-13 12:49:31 +0530

    updated api request calls

--- src/App.js
+++ src/App.js
@@ -1,46 +1,55 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import InputBar from "./Components/InputBar";
-import Temp from "./Components/Temp";
-import TimeAndDate from "./Components/TimeAndDate";
-import TopBar from "./Components/TopBar";
-import getWeatherData from "./Components/Script.js";
-import Cities from "./Components/Cities.jsx";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-  
+import './App.css'
+import { useEffect, useState } from 'react'
+import InputBar from './Components/InputBar'
+import Temp from './Components/Temp'
+import TimeAndDate from './Components/TimeAndDate'
+import TopBar from './Components/TopBar'
+import getWeatherData from './Components/Script.js'
+import Cities from './Components/Cities.jsx'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App() {
-  const [query, setquery] = useState({ q: "Sirsa" });
-  const [weather, setweather] = useState(null);
+    const [query, setquery] = useState({ q: 'Sirsa' })
+    const [weather, setweather] = useState(null)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const message = query.q ? query.q : 'curret location.'
-      toast.info(`Fetching weather for ${message}`)
-      await getWeatherData({ ...query }).then((data) => {
-        toast.success(`Successfully fetched weather data for ${data.name}`)
-        setweather(data)
-      });
-    };
-    fetchData();
-  }, [query]); 
+    useEffect(() => {
+        const fetchData = async () => {
+            const message = query.q ? query.q : 'curret location.'
+            toast.info(`Fetching weather for ${message}`)
+            await getWeatherData({ ...query })
+                .then((data) => {
+                    toast.success(
+                        `Successfully fetched weather data for ${data.name}`
+                    )
+                    setweather(data)
+                })
+                .catch((error) => {
+                    toast.error('City not found')
+                })
+        }
+        fetchData()
+    }, [query])
 
-  return (
-    <div className="mx-auto max-w-screen-md mt-1 mb-10 py-5 px-32 bg-gradient-to-br from-blue-700 to-green-700 h-fit shadow-xl shadow-gray-400 rounded">
-      <TopBar/>
-      <InputBar setquery={setquery} />
+    return (
+        <div className="mx-auto max-w-screen-md mt-1 mb-10 py-5 px-32 bg-gradient-to-br from-blue-700 to-green-700 h-fit shadow-xl shadow-gray-400 rounded">
+            <TopBar />
+            <InputBar setquery={setquery} />
 
-      {weather && (
-        <div>
-          <TimeAndDate weather={weather} />
-          <Temp weather={weather} />
+            {weather && (
+                <div>
+                    <TimeAndDate weather={weather} />
+                    <Temp weather={weather} />
+                </div>
+            )}
+
+            <Cities setquery={setquery} />
+
+            <ToastContainer
+                autoClose={3000}
+                theme="colored"
+                newestOnTop={true}
+            />
         </div>
-      )}
-
-      <Cities setquery={setquery} />
-
-      <ToastContainer autoClose={3000} theme="colored" newestOnTop={true}/>
-    </div>
-  );
+    )
 }
--- src/Components/Script.js
+++ src/Components/Script.js
@@ -1,53 +1,63 @@
-import moment from "moment/moment";
+import axios from 'axios'
+import moment from 'moment/moment'
 
-const apiKey = "bbf5947563682b7c072e5d9861b27bb7";
+const apiKey = 'bbf5947563682b7c072e5d9861b27bb7'
 
 const getWeatherData = async (city) => {
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather")
-  url.search = new URLSearchParams({...city, units: "metric", appid: apiKey,});
-  const response = await fetch(`url`).then((res) => res.json()).then((data) => filterData(data))
-  return response
+    let response
+
+    if (city.q) {
+        response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city.q}&units=metric&appid=${apiKey}`
+        )
+    } else {
+        response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`
+        )
+    }
+
+    return filterData(response.data)
 }
 
 const filterData = (data) => {
-  const {
-    main: { temp, feels_like, temp_min, temp_max, humidity },
-    name,
-    dt,
-    sys: { country, sunrise, sunset },
-    weather,
-    wind: { speed },
-  } = data;
+    const {
+        main: { temp, feels_like, temp_min, temp_max, humidity },
+        name,
+        dt,
+        sys: { country, sunrise, sunset },
+        weather,
+        wind: { speed },
+    } = data
 
-  const { main: details, icon } = weather[0];
+    const { main: details, icon } = weather[0]
 
-  return {
-    temp,
-    feels_like,
-    temp_max,
-    temp_min,
-    humidity,
-    name,
-    dt,
-    country,
-    sunrise,
-    sunset,
-    details,
-    icon,
-    speed,
-  };
-};
+    return {
+        temp,
+        feels_like,
+        temp_max,
+        temp_min,
+        humidity,
+        name,
+        dt,
+        country,
+        sunrise,
+        sunset,
+        details,
+        icon,
+        speed,
+    }
+}
 
 const weatherIcons = (image) =>
-  `http://openweathermap.org/img/wn/${image}@2x.png`;
+    `http://openweathermap.org/img/wn/${image}@2x.png`
 
 const cleanDate = (dt) => {
-  return moment(dt * 1000).format("dddd, MMMM D YYYY");
-};
+    return moment(dt * 1000).format('dddd, MMMM D YYYY')
+}
 
 const cleanTime = (dt) => {
-  return moment(dt * 1000).format("h:mm A");
-};
+    return moment(dt * 1000).format('h:mm A')
+}
 
-export default getWeatherData;
-export { weatherIcons, cleanDate, cleanTime};
+export default getWeatherData
+export { weatherIcons, cleanDate, cleanTime }
